7602cc3a5e810e371f8bf94995b6fdb3fffd4cd8
Matt Schofield <matt.schofield.102@gmail.com>
2014-09-06 11:32:44 +0200

    Finished hack!

--- folk_crawler.js
+++ folk_crawler.js
@@ -5,18 +5,26 @@ var request = require('request')
   , qs = require('qs')
   , fs = require('fs');
 
-var input = fs.readFileSync('/Users/mattsch/Desktop/folkabulary/MY_artists.tsv').toString();
+var input = fs.readFileSync('/Users/mattsch/Desktop/folkabulary/all_artists.tsv').toString();
 
 var apikey = process.env.MUSICGRAPH_KEY
   , searchurl = 'http://api.musicgraph.com/api/v2/track/search';
 
+var artistTrackCounts = {};
 
 async.eachSeries([1,100,200,300,400,500], function (offset, ringback) {
   async.each(input.split('\n'), function (line, callback) {
 
     var name = line.split('\t')[0]
-      , mbid = line.split('\t')[1];
+      , mbid = line.split('\t')[1]
+      , trackCount = 0;
 
+    if (!artistTrackCounts.hasOwnProperty(mbid)) {
+      artistTrackCounts[mbid] = trackCount;
+    } else {
+      artistTrackCounts[mbid] += trackCount;
+    }
+    
     console.log('%s has ID: %s', name, mbid);
 
     var params = {
@@ -44,26 +52,27 @@ async.eachSeries([1,100,200,300,400,500], function (offset, ringback) {
           var lyricsurl = 'http://api.musicgraph.com/api/v2/track/'+track.id+'/lyrical-features?'+qs.stringify(params);
 
           // setTimeout(function () {
-            request(lyricsurl, function (err, res, body) {
-              if (err) {
-                console.log(err);
-                return cb(err);
+          request(lyricsurl, function (err, res, body) {
+            if (err) {
+              console.log(err);
+              return cb(err);
+            } else {
+
+              if (!JSON.parse(body).hasOwnProperty('data') || !JSON.parse(body).data.hasOwnProperty('bow')) {
+                return cb();
               } else {
+                artistTrackCounts[mbid]++;
+                var lyrics = JSON.parse(body).data.bow;
+                var output = path.join('.', 'output/'+name+'_'+mbid);
+                var toWrite = lyrics.join(' ') + ' ';
 
-                if (!JSON.parse(body).hasOwnProperty('data') || !JSON.parse(body).data.hasOwnProperty('bow')) {
-                  return cb();
-                } else {
-                  var lyrics = JSON.parse(body).data.bow;
-                  var output = path.join('.', 'output/'+name+'_'+mbid);
-                  var toWrite = lyrics.join(' ') + ' ';
+                fs.appendFileSync(output, toWrite);
 
-                  fs.appendFileSync(output, toWrite);
-
-                  console.log('Finished writing output for %s', track.title);
-                  return cb();
-                }
+                console.log('Finished writing output for %s', track.title);
+                return cb();
               }
-            })
+            }
+          })
           // }, 3000)
         }, function (err) {
           return callback(err);
@@ -82,6 +91,11 @@ async.eachSeries([1,100,200,300,400,500], function (offset, ringback) {
   if (err) {
     console.log(err);
   }
+
+  Object.keys(artistTrackCounts).forEach(function (key) {
+    var toWrite = key + "\t" + artistTrackCounts[key] + '\n'
+    fs.appendFileSync(path.join('.', 'totals.tsv'), toWrite);
+  })
 
   console.log('All done!');
   process.exit(0);
